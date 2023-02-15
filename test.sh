cb1035fa466a364bec0ba1f9ae7aaeaeeb758868
Maor-Kavod <maork@elementor.com>
2023-02-15 10:22:29 +0200

    fix

--- test.sh
+++ test.sh
@@ -1,3 +1,2 @@
-CURRENT_CHANNEL_VERSION=$(git ls-remote --tags | grep -v "\-rc" | grep "beta" | grep "3.10.2" | tail -n1 | awk -F'beta' '{print $2}')
-
+CURRENT_CHANNEL_VERSION=$(git ls-remote --tags | grep -v "\-rc" | grep "beta" | grep "{{ inputs.CLEAN_PACKAGE_VERSION }}" | tail -n1 | awk -F'beta' '{print $2}')
 echo $CURRENT_CHANNEL_VERSION
